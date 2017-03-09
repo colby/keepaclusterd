f8962e6508d9e8935d4621a13bb27f4050a34415
Colby Olson <me@colbyolson.com>
2017-03-09 12:14:27 -0800

    include nginx vhost conf template

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,6 +1,14 @@
 package %w(nginx)
 service = 'nginx'
 
+template '/etc/nginx/sites-available/default' do
+    source  'vhost.conf.erb'
+    owner   'root'
+    group   'root'
+    mode    '0664'
+    notifies :restart, "service[#{service}]", :delayed
+end
+
 service service do
     action [:enable, :start]
 end
